4276f7be8ada867d389f5d9562031c7a54a84a99
JoshuaDoes <joshuadoespersonalstuff@gmail.com>
2025-01-08 18:53:55 -0500

    pixelstat: Track wlan1

--- pixelstat.go
+++ pixelstat.go
@@ -27,7 +27,7 @@ func main() {
     NewNodeCurrent("/sys/class/power_supply/battery/current_now"),
     NewNodeWattage(),
     NewNodeNetSpeed("/sys/class/net",
-      "wlan0", "rmnet2"),
+      "wlan0", "wlan1", "rmnet2"),
     NewNodeCPUFreq("/sys/devices/system/cpu/cpufreq"),
     NewNodeThermal("/sys/class/thermal",
       "LITTLE", "MID", "BIG",
