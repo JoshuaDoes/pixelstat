521b7e7e322bcc49f2865d8179c17dc5296b27a0
JoshuaDoes <joshuadoespersonalstuff@gmail.com>
2025-01-09 01:53:19 -0500

    pixelstat: Allow disabling thermal nodes, reorganize layout

--- pixelstat.go
+++ pixelstat.go
@@ -12,32 +12,40 @@ var (
   pollingRate int  = 1920
   refreshRate int  = 1920
   novrr       bool = false
+  nothermal   bool = false
 )
 
 func main() {
   pflag.IntVar(&pollingRate, "poll", pollingRate, "max polling rate per node")
   pflag.IntVar(&refreshRate, "rate", refreshRate, "max refresh rate per renderer")
   pflag.BoolVar(&novrr, "novrr", false, "disable variable refresh rate")
+  pflag.BoolVar(&nothermal, "nothermal", false, "disable thermal nodes")
   pflag.Parse()
 
   t := NewTracker(
     NewNodeBattery("/sys/class/power_supply/battery/capacity"),
     NewNodeCharging("/sys/class/power_supply/battery/status"),
-    NewNodeVoltage("/sys/class/power_supply/battery/voltage_now"),
-    NewNodeCurrent("/sys/class/power_supply/battery/current_now"),
-    NewNodeWattage(),
     NewNodeNetSpeed("/sys/class/net",
       "wlan0", "wlan1", "rmnet2"),
     NewNodeCPUFreq("/sys/devices/system/cpu/cpufreq"),
-    NewNodeThermal("/sys/class/thermal",
-      "LITTLE", "MID", "BIG",
-      "G3D", "TPU", "soc",
-      "gnss_tcxo_therm", "disp_therm",
-      "usb_pwr_therm", "usb_pwr_therm2",
-      "qi_therm", "battery", "batt_vs", "maxfg",
-      "neutral_therm", "quiet_therm"),
-    NewNodeRenderer(refreshRate, !novrr),
+    NewNodeVoltage("/sys/class/power_supply/battery/voltage_now"),
+    NewNodeCurrent("/sys/class/power_supply/battery/current_now"),
+    NewNodeWattage(),
   )
+
+  if !nothermal {
+    t.Register(
+      NewNodeThermal("/sys/class/thermal",
+        "LITTLE", "MID", "BIG",
+        "G3D", "TPU", "soc",
+        "gnss_tcxo_therm", "disp_therm",
+        "usb_pwr_therm", "usb_pwr_therm2",
+        "qi_therm", "battery", "batt_vs", "maxfg",
+        "neutral_therm", "quiet_therm"),
+    )
+  }
+
+  t.Register(NewNodeRenderer(refreshRate, !novrr))
   t.Start(pollingRate)
 
   sig := make(chan os.Signal, 1)
